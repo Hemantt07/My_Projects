db4cc1fca01b357aff4ea167a894e1db37be4d55
Hemant Dhiman <hemant@cybertrontechnologies>
2022-12-20 18:26:02 +0530

    20 Dec

--- Calculator/javascript.js
+++ Calculator/javascript.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
 
-	
-	
+	$(".symbol").click(function(){
+
+		var input = $(this).text()
+		var val = $("#output").val();
+
+		if ( input == "AC" ) {
+
+			$("#output").val(" ");
+
+		} else if ( input == "=" ) {
+
+			var math = $("#output").val();
+			var res = eval(math);
+			$("#output").val(res);
+
+		} else if ( input == "⌫" ) {
+
+			var id = $("#output").val();
+			var lastChar = id.substr(id.length - 1);
+			var ind = id.lastIndexOf(lastChar);
+			id = setCharAt( id, ind,'');
+			$("#output").val( id );
+
+		} else {
+
+			var newVal = val + input;
+			$("#output").val( newVal );
+
+		}
+
+	});
+
 });
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
